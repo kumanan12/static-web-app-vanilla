d71ab1930f0648bb682450700f075a6ff85ba70e
Kumanan Murugesan <kumanan12@gmail.com>
2020-08-10 18:04:46 -0400

    calling api from client.

--- api/get-message/index.js
+++ api/get-message/index.js
@@ -8,6 +8,9 @@ module.exports = async function (context, req) {
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        // body: responseMessage
+        body: {
+            text: "Hello from the API"
+          }
     };
 }
